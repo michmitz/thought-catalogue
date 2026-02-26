bc4893bd623be251e5c99eec89c987dc5affa281
Michelle Stermitz <stermitzm@gmail.com>
2026-02-25 18:30:22 -0800

    Basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@
 function App() {
 
   return (
-    <div>
-     <p className="text-red-500">Thought Catalogue</p>
-    </div>
+    <div className="h-screen bg-neutral-950 text-neutral-200 flex items-center justify-center">
+    <h1 className="text-2xl tracking-wide font-light">
+      Thought Catalogue
+    </h1>
+  </div>
   )
 }
 
